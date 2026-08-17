dc9b6f249f9830476a7c8dd82f62793553b3106f
Anisha Agrawal <anishasa525@gmail.com>
2026-08-17 10:40:17 +0530

    Add resume upload endpoint: multer + Cloudinary storage, saves record to MongoDB

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,5 @@
 const { AppError, asyncHandler } = require("../middleware/errorHandler");
+const ResumeAnalysis = require("../models/ResumeAnalysis");
 
 const uploadResume = asyncHandler(async (req, res) => {
   if (!req.file) {
@@ -7,9 +8,15 @@ const uploadResume = asyncHandler(async (req, res) => {
 
   const resumeUrl = req.file.path;
 
+  const resumeDoc = await ResumeAnalysis.create({
+    userId: req.user.id,
+    resumeUrl: resumeUrl,
+  });
+
   res.status(201).json({
     message: "Resume uploaded successfully",
     resumeUrl,
+    resumeId: resumeDoc._id,
   });
 });
 
